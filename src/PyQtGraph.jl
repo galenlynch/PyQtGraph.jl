02a34b0a3708c33baf0c18a3730eea50ac3b1983
Galen Lynch <galen.lynch@gmail.com>
2018-06-13 15:57:11 -0400

    Use Qt5

--- src/PyQtGraph.jl
+++ src/PyQtGraph.jl
@@ -22,12 +22,14 @@ export
 const pg = PyNULL()
 const qtc = PyNULL()
 const qtg = PyNULL()
+const qt5 = PyNULL()
 
 include("util.jl")
 include("app.jl")
 include("subplots.jl")
 
 function __init__()
+    copy!(qt5, pyimport("PyQt5"))
     copy!(pg, pyimport("pyqtgraph"))
     copy!(qtc, pg[:Qt][:QtCore])
     copy!(qtg, pg[:Qt][:QtGui])
