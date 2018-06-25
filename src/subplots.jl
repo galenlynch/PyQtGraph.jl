46f7e0112b9051933885d888c2aac403afa15dd4
Galen Lynch <galen.lynch@gmail.com>
2018-06-25 11:52:17 -0400

    Add option to specify titles in subplot grid

--- src/subplots.jl
+++ src/subplots.jl
@@ -1,13 +1,15 @@
 layout_widget() =  pg[:GraphicsLayoutWidget]()
 
-function linked_subplot_grid(win::PyObject, args::Vararg{AbstractArray})
-    plot_handles = subplot_grid(win, args...)
+function linked_subplot_grid(
+    win::PyObject, args::Vararg{AbstractArray}; kwargs...
+)
+    plot_handles = subplot_grid(win, args...; kwargs...)
     link_x_dims(plot_handles)
     plot_handles
 end
-function linked_subplot_grid(args::Vararg{AbstractArray})
+function linked_subplot_grid(args::Vararg{AbstractArray}; kwargs...)
     win = layout_widget()
-    (win, linked_subplot_grid(win, args...))
+    (win, linked_subplot_grid(win, args...; kwargs...))
 end
 
 function link_x_dims(handles::AbstractArray{PyObject})
@@ -19,13 +21,16 @@ end
 
 function subplot_grid(
     layout::PyObject,
-    rowspec::AbstractArray{<:Integer} = [1]
+    rowspec::AbstractArray{<:Integer} = [1];
+    titles::AbstractVector{<:AbstractString} = Vector{String}()
 )
     n_row = length(rowspec)
     plot_handles = Vector{PyObject}(n_row)
+    use_titles = length(titles) == n_row
     for (i, row_height) in enumerate(rowspec)
+        kwargs = use_titles ? ((:title, titles[i]),) : ()
         i > 1 && layout[:nextRow]()
-        plot_handles[i] = layout[:addPlot]()
+        plot_handles[i] = layout[:addPlot](;kwargs...)
         layout[:ci][:layout][:setRowStretchFactor](i - 1, row_height)
     end
     plot_handles
